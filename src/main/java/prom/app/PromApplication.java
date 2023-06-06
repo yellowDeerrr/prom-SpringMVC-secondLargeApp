8efd60e2a9fa0c917fcf779b3a24d13afe2a56b3
yellowDeerrr <emailforgithubbanksystemsql@gmail.com>
2023-06-06 17:37:13 +0300

    first commit

---
+++ src/main/java/prom/app/PromApplication.java
@@ -0,0 +1,13 @@
+package prom.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromApplication.class, args);
+    }
+
+}
---
+++ src/test/java/prom/app/prom/PromApplicationTests.java
@@ -0,0 +1,13 @@
+package prom.app.prom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
